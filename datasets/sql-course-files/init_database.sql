625954ddaa2a8aa0c2db56e7d504383a333dddab
mhdshamil2005 <muhammadshamil005@gmail.com>
2025-07-21 01:26:02 +0530

    Create init_database.sql

---
+++ datasets/sql-course-files/init_database.sql
@@ -0,0 +1,13 @@
+--CREATE DATABASE & SCHEMAS--
+
+CREATE DATABASE DataWarehouse;
+Use DataWarehouse;
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
+---------------
+
